5515bb09390a6e656dc35af20cf6545a545de109
Klaudia Adamus <adamus.klaudia.pasp@gmail.com>
2019-10-16 16:23:23 +0200

    main page with links implemented

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+TEMPLATES_ROUTES = {"main_page" : "main.html",
+                    "leaderboard" : "leaderboard.html",
+                    "new quiz" : "new_quiz.html",
+                    "quiz list": "quiz_list.html"}
+
+@app.route('/')
+def main_page():
+    return render_template(TEMPLATES_ROUTES["main_page"])
+
+
+@app.route('/new-quiz', methods = ["GET"])
+def new_quiz_route():
+    if request.method == "GET":
+        return render_template(TEMPLATES_ROUTES["new quiz"])
+
+
+@app.route('/quiz-list', methods = ["GET"])
+def quiz_list():
+    if request.method == "GET":
+        return render_template(TEMPLATES_ROUTES["quiz list"])
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
