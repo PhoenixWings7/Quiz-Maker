9787fa8bc98c0daf7fad4c473ad64147295734b1
Klaudia Adamus <adamus.klaudia.pasp@gmail.com>
2019-10-17 14:30:47 +0200

    data handling implemented

---
+++ data_handler.py
@@ -0,0 +1,42 @@
+import csv
+
+NUM_OF_QUESTIONS = 4
+def create_data_headers():
+    answer_ids = ["answer_" + str(ord_num) for ord_num in range(2, NUM_OF_QUESTIONS + 1)]
+    data_headers = ["question", "correct_answer"] + answer_ids
+    return data_headers
+
+
+DATA_HEADERS = create_data_headers()
+
+
+def create_new_csv_file(filename, headers):
+    with open(filename, "w") as file:
+        data_dict = csv.DictWriter(file, headers)
+        data_dict.writeheader()
+    return
+
+
+def create_new_quiz_file(quiz_data, quiz_title):
+    filename = quiz_title.lower()
+    create_new_csv_file(filename, DATA_HEADERS)
+    with open(filename, "a") as file:
+        data = csv.DictWriter(file, DATA_HEADERS)
+        data.writerow(quiz_data)
+    return
+
+
+def save_dict_data_to_file(filename, dict_data):
+    with open(filename, "a") as file:
+        data = csv.DictWriter(file, DATA_HEADERS)
+        data.writerow(dict_data)
+    return
+
+
+def add_question_to_file(question_data, quiz_title):
+    filename = quiz_title.lower()
+    try:
+        save_dict_data_to_file(filename, question_data)
+    except FileNotFoundError:
+        create_new_quiz_file(question_data, quiz_title)
+    return
--- server.py
+++ server.py
@@ -1,30 +1,45 @@
 from flask import Flask, render_template, request, redirect, url_for
+import data_handler
 
 app = Flask(__name__)
 
 TEMPLATES_ROUTES = {"main_page" : "main.html",
                     "leaderboard" : "leaderboard.html",
                     "new quiz start" : "new_quiz.html",
-                    "new quiz form" : "new_quiz_form.html",
+                    "next question form" : "next_question.html",
                     "quiz list": "quiz_list.html"}
+
+
 
 @app.route('/')
 def main_page():
     return render_template(TEMPLATES_ROUTES["main_page"])
 
 
-@app.route('/new-quiz', methods = ["GET"])
+@app.route('/new-quiz', methods = ["GET", "POST"])
 def new_quiz_route():
     if request.method == "GET":
-        return render_template(TEMPLATES_ROUTES["new quiz start"])
+        answer_ids = ["answer_" + str(ord_num) for ord_num in range(2, data_handler.NUM_OF_QUESTIONS + 1)]
+        return render_template(TEMPLATES_ROUTES["new quiz start"], answer_ids = answer_ids)
 
-
-@app.route('/new-quiz-form', methods = ["GET", "POST"])
-def new_quiz_form():
-    if request.method == "GET":
-        return render_template(TEMPLATES_ROUTES["new quiz form"])
     if request.method == "POST":
-        pass
+        quiz_title = request.form["quiz_title"]
+        question_data = dict(request.form)
+        question_data.pop("quiz_title")
+
+        data_handler.add_question_to_file(question_data, quiz_title)
+        return redirect(url_for("next_question_form"))
+
+
+@app.route('/new-quiz-next', methods = ["GET", "POST"])
+def next_question_form():
+    if request.method == "GET":
+        quiz_title =
+        answer_ids = ["answer_" + str(ord_num) for ord_num in range(2, data_handler.NUM_OF_QUESTIONS + 1)]
+        return render_template(TEMPLATES_ROUTES["next question form"],quiz_title = quiz_title, answer_ids = answer_ids)
+    if request.method == "POST":
+        answer_ids = ["answer_" + str(ord_num) for ord_num in range(2, data_handler.NUM_OF_QUESTIONS + 1)]
+        return render_template(TEMPLATES_ROUTES["next question form"], answer_ids = answer_ids)
 
 
 @app.route('/quiz-list', methods = ["GET"])
