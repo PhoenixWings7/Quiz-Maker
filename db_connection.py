022703ce8406527b5cc69e822934984067ac36df
Klaudia Adamus <adamus.klaudia.pasp@gmail.com>
2019-11-28 14:02:57 +0100

    important variable name fixed

--- db_connection.py
+++ db_connection.py
@@ -4,8 +4,7 @@ import psycopg2, psycopg2.extras, psycopg2.errors
 
 
 def create_connection_string():
-    db_url = os.environ.get("DATABASE_URLI")
-    print(db_url)
+    db_url = os.environ.get("DATABASE_URL")
     env_var_defined = db_url
 
     if env_var_defined:
